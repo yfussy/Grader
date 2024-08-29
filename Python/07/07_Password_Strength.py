3ea0b2d425bc74d46c6fa03b7ebcf8946d1fecf0
yfussy <167310929+yfussy@users.noreply.github.com>
2024-08-29 14:47:41 +0700

    Update 07_Password_Strength.py

--- Python/07/07_Password_Strength.py
+++ Python/07/07_Password_Strength.py
@@ -24,18 +24,37 @@ def charRep(p):
 def numSeq(p):
     p = p.lower()
     numSeq = ''.join([str(x) for x in range(10)] + ['0'])
-    numSeqRe = ''.join(numSeq[::-1])
+    numSeqRe = numSeq[::-1]
     for i in range(len(p) - 3):
         text = p[i:i+4] 
-        if text in numSeq or text in numSeqRe:
-            return True
+        for seq in numSeq + numSeqRe:
+            if text in seq:
+                return True
     return False
 
 def charSeq(p):
-    pass
-
+    p = p.lower()
+    charSeq = ''.join([chr(x) for x in range(97,123)] + ['a'])
+    charSeqRe = charSeq[::-1]
+    for i in range(len(p) - 3):
+        text = p[i:i+4]
+        for seq in charSeq + charSeqRe:
+            if text in seq:
+                return True
+    return False
+    
 def keyPat(p):
-    pass
+    p = p.lower()
+    keySeq = ['!@#$%^&*()_+', 'qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+    keySeqRe = []
+    for pat in keySeq:
+        keySeqRe.append(pat[::-1])
+    for i in range(len(p) - 3):
+        text = p[i:i+4] 
+        for pat in keySeq + keySeqRe:
+            if text in pat:
+                return True
+    return False
 
 pw = input().split()
 errors = []
