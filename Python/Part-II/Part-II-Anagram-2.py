6b3306032db745142fc31c7e6bfe3ff9ad8ac2c3
yfussy <167310929+yfussy@users.noreply.github.com>
2024-10-16 13:23:35 +0700

    Update Part-II-Anagram-2.py

--- Python/Part-II/Part-II-Anagram-2.py
+++ Python/Part-II/Part-II-Anagram-2.py
@@ -8,12 +8,12 @@ def hashTable(text):
 def showRemove(table):
     if sum(table.values()) == 0:
         print(' - None')
+        return 
     for char, n in table.items():
-        if n != 0:
-            remove = f' - remove {str(n)} {char}'
-            if n > 1:
-                remove += "'s"
-            print(remove)
+        remove = f' - remove {str(n)} {char}'
+        if n > 1:
+            remove += "'s"
+        print(remove)
 
 a = input()
 b = input()
