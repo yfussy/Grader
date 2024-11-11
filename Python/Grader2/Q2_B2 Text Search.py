af1c04203f1005a384b205b681e5b6bc00445df1
yfu <toainth@gmail.com>
2024-11-11 20:25:30 +0700

    optimized

--- Python/Grader2/Q2_B2 Text Search.py
+++ Python/Grader2/Q2_B2 Text Search.py
@@ -3,14 +3,14 @@ n = int(input())
 layers = []
 for _ in range(n):
     layers.append(input())
-S = '|'.join(layers)
+S = '\n'.join(layers)
 
 pointer = 0
 while True:
-    start = S.replace('|', ' ').lower().find(target, pointer)
+    start = S.replace('\n', ' ').lower().find(target, pointer)
     if start == -1:
         break
     pointer = start + len(target)
     S = S[:start] + "<found>" + S[start:pointer] + "</found>" + S[pointer:]
     pointer += 15
-print(*S.split('|'), sep='\n')
+print(S)
